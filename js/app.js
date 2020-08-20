e460e11176c8c8d46563b6f30e5ac4941bb42cf8
Luay Alali <luayalali@gmail.com>
2020-08-20 16:51:44 -0700

    added Promise.all to fetch all data

--- js/app.js
+++ js/app.js
@@ -13,14 +13,19 @@ function fetchData(url){
 
 }
 
+Promise.all([
+    fetchData('https://dog.ceo/api/breeds/list'),
+    fetchData('https://dog.ceo/api/breeds/image/random')
+])
+.then(data => {
+        const breedList = data[0].message;
+        const randomImage = data[1].message;
 
-fetchData('https://dog.ceo/api/breeds/list')
-.then(data => generateOptions(data.message))
+        generateOptions(breedList);
+        generateImage(randomImage);
+})
 
 
-
-fetchData('https://dog.ceo/api/breeds/image/random')
-.then(data => generateImage(data.message))
 
 // ------------------------------------------
 //  HELPER FUNCTIONS
